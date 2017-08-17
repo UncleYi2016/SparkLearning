c7ca42031bf90ba84162615ad1452e8bc81e0faa
ZTom <yizhaopu2008@163.com>
2017-08-17 14:15:23 +0800

    new branch

--- src/com/SparkLearning/test.java
+++ src/com/SparkLearning/test.java
@@ -24,6 +24,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import com.SparkLearning.Controller.LoadHBaseData;
 import com.SparkLearning.Controller.WordCount;
+import com.SparkLearning.Model.Words;
 import com.SparkLearning.TextSegmentation.CustomizeAnalyzer;
 
 
@@ -54,13 +55,14 @@ public class test {
 //		String words = "期待此吧有校友有哪些是剑阁的？衡水哪里有租赁或者卖卡通形象的衣服的，就是那种充满了气人站在里面的那种，哪里有大头娃娃啊，带在头上 卡通衣服兔子最好，没有 的话别的也可以，全东莞最有影响力教育机构全能找到，家教信息免费发布。东莞培训通www.dgpxt.com 哈，标题要亮、亮、亮… 明天就情人节了，祝天下有情人终成眷属…有句话是这样说的 不爱的爱情永远不会变坏 我们调情 我们暧昧 但是我们不爱第一:什么时候开学?!要准确了!!!! 第二:怎么查成绩!这都好久!";
 		List<String> testList = new ArrayList<String>();
 		testList = LoadHBaseData.LoadPostInfo(loadNum);
+//		testList.add(words);
 		List<String> tempList = new ArrayList<String>();
-		List<String> testMap = CustomizeAnalyzer.addCustomizeAnalyzer(testList);
+		List<Words> testMap = CustomizeAnalyzer.wordCountCustomizeAnalyzer(testList);
 		int count = 1;
-		for(String key : testMap){
-			out.write(key + "\n");
-			System.out.println("Writing no. " + count++);
-//			System.out.println(key);
+		for(Words key : testMap){
+			out.write(key.getWord() + "..." + key.getCount() + "\n");
+//			System.out.println("Writing no. " + count++);
+//			System.out.println(key.getWord() + "..." + key.getCount() + "\n");
 		}
 		out.close();
 		System.out.println("FINISHED!");
